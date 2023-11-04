a866f9c6708656edf254093480f65798e20de6d6
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-04 08:49:42 +0200

    Update D. Xor.cpp

--- D. Xor.cpp
+++ D. Xor.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+    long long a, b, n;
+    cin >> a >> b >> n;
+
+    if (n % 3 == 0)
+    {
+        cout << (a ^ b) << endl;
+    }
+    else if (n % 3 == 1)
+    {
+        cout << a << endl;
+    }
+    else if (n % 3 == 2)
+    {
+        cout << b << endl;
+    }
+}
