f516399f3299da301403f467d6dee7d41fe30b75
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-04 07:12:11 +0200

    Update B. Prime checking.cpp

--- B. Prime checking.cpp
+++ B. Prime checking.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << "NO";
+        return 0;
+    }
+
+    int m = sqrt(n);
+
+    for (int i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << "YES";
+    return 0;
+}
