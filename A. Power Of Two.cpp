dad59bf58a05070cb2d23bd84fbf7496f21fccc7
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-04 06:55:23 +0200

    Update A. Power Of Two.cpp

--- A. Power Of Two.cpp
+++ A. Power Of Two.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+    long long  x; 
+    cin >> x;
+    
+    int count_bits = 0;
+
+    while (x > 0)
+    {
+        count_bits += x & 1;
+        x >>= 1;
+    }
+   
+
+    if (count_bits==1)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
+}
