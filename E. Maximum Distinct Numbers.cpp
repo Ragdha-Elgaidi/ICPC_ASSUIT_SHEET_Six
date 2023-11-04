707235c76dcfb835081c4e1e093ccebd19c34cab
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-04 10:17:44 +0200

    Update E. Maximum Distinct Numbers.cpp

--- E. Maximum Distinct Numbers.cpp
+++ E. Maximum Distinct Numbers.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+#define ll  long long
+
+using namespace std;
+
+int main()
+{
+    ll num;
+    cin >> num;
+
+    ll k = (-1 + sqrt(1 + 8 * num)) / 2;
+    cout << k << endl;
+
+    return 0;
+}
+
