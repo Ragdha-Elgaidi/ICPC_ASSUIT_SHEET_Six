e5b4205cc0888ec180f688df1cfbedd0f7856b2c
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-06 22:15:25 +0200

    Update G. Summation of its divisors.cpp

--- G. Summation of its divisors.cpp
+++ G. Summation of its divisors.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+int main()
+{
+    Ragdha
+
+    ll n,sum=0;
+
+    cin >> n;
+
+    for (int i = 1; i <= sqrt(n); i++ )
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+            if (i != sqrt(n))
+            {
+                sum +=n/i;
+            }
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
