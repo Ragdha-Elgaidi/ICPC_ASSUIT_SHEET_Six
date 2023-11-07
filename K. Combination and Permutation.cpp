1f7f7142236f24ba0a7ed6b002db88075c6ff49c
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-07 05:27:41 +0200

    Update K. Combination and Permutation.cpp

--- K. Combination and Permutation.cpp
+++ K. Combination and Permutation.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+// Function to calculate factorial
+ll factorial(int n) 
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() 
+{
+    Ragdha
+
+    ll A, B;
+    cin >> A >> B;
+
+    // Calculate nCr and nPr using the formulas
+    ll nCr = factorial(A) / (factorial(B) * factorial(A - B));
+    ll nPr = factorial(A) / factorial(A - B);
+
+    // Output the results separated by a space
+    cout << nCr << " " << nPr << endl;
+
+    return 0;
+}
