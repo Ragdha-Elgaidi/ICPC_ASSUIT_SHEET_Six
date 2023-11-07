a95eed6c8439dce4b14c659a32900a5bcb42a076
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-07 05:58:31 +0200

    Update M. Divisible.cpp

--- M. Divisible.cpp
+++ M. Divisible.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+int main()
+{
+	Ragdha
+
+	string num1;
+	int num2;  
+
+	cin >> num1 >> num2;
+
+	ll res = 0;
+
+	for (int i = 0; i < num1.size(); i++)
+	{ 
+		res *= 10;
+		res += num1[i] - '0';
+		res %= num2;
+	}
+
+	if(res == 0) 
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+
+	return 0;
+}
