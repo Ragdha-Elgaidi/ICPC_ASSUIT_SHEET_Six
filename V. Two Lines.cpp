147a0b0d0d2fd99f3f0255d4767f9511d6908c66
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-11 11:07:18 +0200

    Update V. Two Lines.cpp

--- V. Two Lines.cpp
+++ V. Two Lines.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+int main()
+{
+    Ragdha
+
+    // Input coordinates of the start and end points for two lines
+    ll X1, Y1, X2, Y2;
+    ll X3, Y3, X4, Y4;
+
+    cin >> X1 >> Y1 >> X2 >> Y2;
+    cin >> X3 >> Y3 >> X4 >> Y4;
+
+    // Calculate slopes for each line
+    ll slope1 = (Y2 - Y1) * (X4 - X3);
+    ll slope2 = (Y4 - Y3) * (X2 - X1);
+
+    // Check if slopes are approximately equal
+    if (slope1 == slope2) 
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
